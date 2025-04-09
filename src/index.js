40ab4071b199d4b0dc3581f29184123eb43902fc
fiftythanks <mikhail.sholokhov@tutamail.com>
2025-04-09 17:56:46 +0700

    chore: complete 3 exs in "Testing Practice"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-import './style.css';
-
-function sum(a, b) {
-  return a + b;
+export function capitalize(string) {
+  return string.replace(string.at(0), string.at(0).toUpperCase());
 }
 
-module.exports = sum;
+export function reverseString(string) {
+  return string.split('').reverse().join('');
+}
+
+export const calculator = {
+  subtract(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number')
+      throw new Error('Input must be two numbers!');
+    return a - b;
+  },
+  divide(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number')
+      throw new Error('Input must be two numbers!');
+    return a / b;
+  },
+  multiply(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number')
+      throw new Error('Input must be two numbers!');
+    return a * b;
+  },
+};
--- src/index.test.js
+++ src/index.test.js
@@ -1,93 +1,70 @@
-const sum = require('./index.js');
+import { capitalize, reverseString, calculator } from './index';
 
-it('adds 1 + 2 to equal 3', () => {
-  expect(sum(5, 2)).toBe(7);
+describe('capitalize', () => {
+  it('returns capitalized input string', () => {
+    expect(capitalize('test')).toBe('Test');
+  });
+
+  it("throws if input isn't a string", () => {
+    expect(() => capitalize(1)).toThrow();
+    expect(() => capitalize(true)).toThrow();
+    expect(() => capitalize([1, 2])).toThrow();
+    expect(() => capitalize({ a: 1, b: 2 })).toThrow();
+  });
 });
 
-test('object assignment', () => {
-  const data = { one: 1 };
-  data['two'] = 2;
-  expect(data).toEqual({ one: 1, two: 2 });
+describe('reverseString', () => {
+  it('returns reversed input string', () => {
+    expect(reverseString('test')).toBe('tset');
+  });
+
+  it("throws if input isn't a string", () => {
+    expect(() => reverseString(1)).toThrow();
+    expect(() => reverseString(true)).toThrow();
+    expect(() => reverseString([1, 2])).toThrow();
+    expect(() => reverseString({ a: 1, b: 2 })).toThrow();
+  });
 });
 
-test('adding positive numbers is not zero', () => {
-  for (let a = 1; a < 10; a++) {
-    for (let b = 1; b < 10; b++) {
-      expect(a + b).not.toBe(0);
-    }
-  }
-});
-
-test('null', () => {
-  const n = null;
-  expect(n).toBeNull();
-  expect(n).toBeDefined();
-  expect(n).not.toBeUndefined();
-  expect(n).not.toBeTruthy();
-  expect(n).toBeFalsy();
-});
-
-test('zero', () => {
-  const z = 0;
-  expect(z).not.toBeNull();
-  expect(z).toBeDefined();
-  expect(z).not.toBeUndefined();
-  expect(z).not.toBeTruthy();
-  expect(z).toBeFalsy();
-});
-
-test('two plus two', () => {
-  const value = 2 + 2;
-  expect(value).toBeGreaterThan(3);
-  expect(value).toBeGreaterThanOrEqual(3.5);
-  expect(value).toBeLessThan(5);
-  expect(value).toBeLessThanOrEqual(4.5);
-
-  // toBe and toEqual are equivalent for numbers
-  expect(value).toBe(4);
-  expect(value).toEqual(4);
-});
-
-test('adding floating point numbers', () => {
-  const value = 0.1 + 0.2;
-  //expect(value).toBe(0.3); -- This won't work because of rounding error
-  expect(value).toBeCloseTo(0.3);
-});
-
-test('there is no I in team', () => {
-  expect('team').not.toMatch(/I/);
-});
-
-test('but there is a "stop" in Christoph', () => {
-  expect('Christoph').toMatch(/stop/);
-});
-
-const shoppingList = [
-  'diapers',
-  'kleenex',
-  'trash bags',
-  'paper towels',
-  'milk',
-];
-
-test('the shopping list has milk on it', () => {
-  expect(shoppingList).toContain('milk');
-  expect(new Set(shoppingList)).toContain('milk');
-});
-
-function compileAndroidCode() {
-  throw new Error('you are using the wrong JDK!');
-}
-
-test('compiling android goes as expected', () => {
-  expect(() => compileAndroidCode()).toThrow();
-  expect(() => compileAndroidCode()).toThrow(Error);
-
-  // You can also use a string that must be contained in the error message or a regex
-  expect(() => compileAndroidCode()).toThrow('you are using the wrong JDK');
-  expect(() => compileAndroidCode()).toThrow(/JDK/);
-
-  // Or you can match an exact error message using a regexp like below
-  // expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK$/); -- fails
-  expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK!$/);
+// A calculator object that contains functions for the basic operations: add, subtract, divide, and multiply. Each of these functions should take two numbers and return the correct calculation.
+describe('calculator', () => {
+  describe('subtract', () => {
+    it('returns the first number minus the second one', () => {
+      expect(calculator.subtract(1, 5)).toBe(-4);
+    });
+    it("throws if input isn't two numbers", () => {
+      expect(() => calculator.subtract(1)).toThrow();
+      expect(() => calculator.subtract(true)).toThrow();
+      expect(() => calculator.subtract([1, 2])).toThrow();
+      expect(() => calculator.subtract({ a: 1, b: 2 })).toThrow();
+      expect(() => calculator.subtract('string')).toThrow();
+      expect(() => calculator.subtract('string', 'string')).toThrow();
+    });
+  });
+  describe('divide', () => {
+    it('returns the first number divided by the second one', () => {
+      expect(calculator.divide(10, 5)).toBe(2);
+    });
+    it("throws if input isn't two numbers", () => {
+      expect(() => calculator.divide(1)).toThrow();
+      expect(() => calculator.divide(true)).toThrow();
+      expect(() => calculator.divide([1, 2])).toThrow();
+      expect(() => calculator.divide({ a: 1, b: 2 })).toThrow();
+      expect(() => calculator.divide('string')).toThrow();
+      expect(() => calculator.divide('string', 'string')).toThrow();
+    });
+  });
+  describe('multiply', () => {
+    it('returns the first number times the second one', () => {
+      expect(calculator.multiply(10, 5)).toBe(50);
+    });
+    it("throws if input isn't two numbers", () => {
+      expect(() => calculator.multiply(1)).toThrow();
+      expect(() => calculator.multiply(true)).toThrow();
+      expect(() => calculator.multiply([1, 2])).toThrow();
+      expect(() => calculator.multiply({ a: 1, b: 2 })).toThrow();
+      expect(() => calculator.multiply('string')).toThrow();
+      expect(() => calculator.multiply('string', 'string')).toThrow();
+    });
+  });
 });
